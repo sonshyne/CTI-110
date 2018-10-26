b1a5dc1fe7c3ed9a7862dce8a65c2c1a58bdcc9b
sonshyne <44252992+sonshyne@users.noreply.github.com>
2018-10-25 20:54:11 -0400

    Add files via upload

--- P2HW1_CelciusConverter_DeborahCroom.py
+++ P2HW1_CelciusConverter_DeborahCroom.py
@@ -1,16 +1,14 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> #Celcius to Fahrenheit Converter (Chapter 2 - Exercise 9) Python 2
->>> #10/14/2018
->>> #CTI 110 P2HW1 - Celcius Fahrenheit Converter
->>> #Deborah Croom
->>> 
->>> #Input temperature degrees in Celcius
->>> #Calculate fahrenheit as (9/5) * celcius + 32
->>> #Output fahrenheit
->>> 
->>> celcius = 32
->>> fahrenheit = (9.0/5.0) * celcius + 32
->>> print (celcius, "Celcius is", fahrenheit, "Fahrenheit.")
-32 Celcius is 89.6 Fahrenheit.
->>> 
+
+#Celcius to Fahrenheit Converter (Chapter 2 Exercise 9)
+#10/24/18
+#CTI 110 P2HW1 - Celcius Fahrenheit Converter
+#Deborah Croom
+
+#Input temperature degrees in Celcius
+#Calculate fahrenheit as (9/5) * celcius + 32
+#Output fahrenheit
+
+celcius=int(input("What is the Celcius temperature?"))
+fahrenheit=(9/5)*celcius + 32
+print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
--- P2HW2_MaleFemalePercentage_DeborahCroom.py
+++ P2HW2_MaleFemalePercentage_DeborahCroom.py
@@ -1,25 +1,21 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
->>> #Write a program that asks for the # of males and females in a class and display the percentage
->>> #10/17/18
->>> #CTI-110 P2HW2 - Male Female Percentage
->>> #Deborah Croom
->>> 
->>> #Suppose there are 8 males and 12 females in a class
->>> #There are 20 students in a class
->>> #Percentage of males calculated as 8/20=40%
->>> #Percentage of females calculated as 12/20=60%
->>> 
->>> males=int(input("Enter the number of males in the class:"))
-Enter the number of males in the class:8
->>> females=int(input("Enter the number of females in the class:"))
-Enter the number of females in the class:12
->>> totalStudents=males+females
->>> malePercentage=(males/totalStudents)*100
->>> femalePercentage=(females/totalStudents)*100
->>> print("Male percentage:"+str(malePercentage))
-Male percentage:40.0
->>> print("Female percentage:"+str(femalePercentage))
-Female percentage:60.0
->>> 
+
+#Write a program that asks for the # of males and females in a class and display the percentage
+#10/24/18
+#CTI-110 P2HW2 - Male Female Percentage
+#Deborah Croom
+
+#Suppose there are 8 males and 12 females in a class
+#There are 20 students in a class
+#Percentage of males is calculated as 8/20=40%
+#Percentage of females calculated as 12/20=60%
+
+males = int(input("Enter the number of males in the class:"))
+females = int(input("Enter the number of females in the class:"))
+totalStudents = males + females
+
+malePercentage = (males/totalStudents) * 100
+femalePercentage = (females/totalStudents) * 100
+
+print("Male percentage: " + str(malePercentage))
+print("Female percentage: " + str(femalePercentage))
+
