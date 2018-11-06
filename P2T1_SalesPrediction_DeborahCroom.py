082c8efd037ae5643111c509c4d5fa7a4c9d0b45
sonshyne <44252992+sonshyne@users.noreply.github.com>
2018-11-05 21:09:42 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_DeborahCroom.py
@@ -0,0 +1,16 @@
+
+#Sales Prediction (Chapter 2 - Exercise 2)
+#11/5/2018
+#CTI-110 P2T1 - Sales Prediction
+#Deborah Croom
+
+#Get the total sales
+#Calulate the total profit
+#Display the profit
+
+
+total_sales=float(input('Enter the projected sales:'))
+profit=total_sales * 0.23
+print('The profit is $',profit)
+
+
